d055ca7c638e755e69653786849192212f6df77b
fxpw <fxpw@outlook.com>
2024-09-05 14:15:02 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,18 +21,19 @@ changed_files=$(git diff --name-only "origin/$BASE_BRANCH" "$GITHUB_SHA")
 
 need_throw_error=false
 for filename in $changed_files; do
-	if [[ "$filename" == *.blade.php ]]; then
-		line_number=0
-		while IFS= read -r line; do
-			line_number=$((line_number + 1))
-			if contains_old_php_format "$line"; then
-				echo "File ${filename}:${line_number} Line \"$line\""
-				need_throw_error=true
-			fi
-		done <"$filename"
+	if [[ -f "$filename" ]]; then
+		if [[ "$filename" == *.blade.php ]]; then
+			line_number=0
+			while IFS= read -r line; do
+				line_number=$((line_number + 1))
+				if contains_old_php_format "$line"; then
+					echo "File ${filename}:${line_number} Line \"$line\""
+					need_throw_error=true
+				fi
+			done <"$filename"
+		fi
 	fi
 done
-
 
 if $need_throw_error; then
 	echo "Find files whit old template."
